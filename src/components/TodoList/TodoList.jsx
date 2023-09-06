483919d8c5d81211615448bb137023922b16cd00
soyeon <soylee57@naver.com>
2023-09-07 02:52:36 +0900

    Update: 로컬 스토리지에 목록 데이터 저장

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,31 +1,19 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { GoTrash } from 'react-icons/go';
 import Add from '../Add/Add';
 import Todo from '../Todo/Todo';
 
 export default function TodoList({ filter }) {
-  const [todos, setTodos] = useState([
-    {
-      id : '123',
-      text: '1day planner 만들기',
-      status: 'todo',
-      startTime: '',
-      endTime: ''
-    },
-    {
-      id : '234',
-      text: '점심 먹기',
-      status: 'todo',
-      startTime: '',
-      endTime: ''
-    },
-  ])
+  const [todos, setTodos] = useState(getTodosFromLocalStorage);
 
   const addTodo = (newTodo) => {setTodos(todos => [...todos, newTodo]);}
   const deleteTodo = (deleted) => {setTodos(todos => todos.filter(todo => todo.id !== deleted.id))}
   const updateTodo = (updated) => {setTodos(todos => todos.map(todo => todo.id === updated.id ? updated : todo))};
-
   const deleteAll = () => {setTodos(todos => todos.filter(todo => todo.status === 'todo'))};
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos])
 
   const uncompletedTodos = filterTodos(todos, 'todo');
   const completedTodos = filterTodos(todos, 'completed');
@@ -52,6 +40,11 @@ export default function TodoList({ filter }) {
 
 function filterTodos(todos, filter) {
   return todos.filter(todo => todo.status === filter);
+}
+
+function getTodosFromLocalStorage() {
+  const todos = localStorage.getItem('todos');
+  return todos ? JSON.parse(todos) : [];
 }
   
 
