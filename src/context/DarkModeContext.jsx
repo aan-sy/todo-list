943ad4a4625c3b5f5dc4be53efda910345ba160a
soyeon <soylee57@naver.com>
2023-09-07 02:26:49 +0900

    Update: 로컬 스토리지에 다크 모드 데이터 저장

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 export const DarkModeContext = createContext();
 
@@ -8,6 +8,17 @@ export const DarkModeProvider = ({ children }) => {
     setDarkMode(darkMode => !darkMode);
     updateMode(!darkMode)
   }
+
+  useEffect(() => {
+    const isDark = 
+      localStorage.theme === 'dark' || 
+      (
+        !('theme' in localStorage) &&
+        window.matchMedia(`(prefers-color-scheme: dark)`).matches
+      )
+    setDarkMode(isDark);
+    updateMode(isDark);
+  }, [])
 
   return <DarkModeContext.Provider value={{darkMode, toggleDarkMode}}>
     {children}
@@ -19,7 +30,9 @@ export const useDarkMode = () => useContext(DarkModeContext);
 const updateMode = (darkMode) => {
   if (darkMode) {
     document.documentElement.classList.add('darkMode');
+    localStorage.theme = 'dark';
   } else {
     document.documentElement.classList.remove('darkMode');
+    localStorage.theme = 'light';
   }
 }
